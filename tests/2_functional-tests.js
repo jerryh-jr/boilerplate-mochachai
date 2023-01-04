d1f18c161116312db7fab1ae1ef702c9ec77fcc8
Jerry <47369872+jerryh-jr@users.noreply.github.com>
2023-01-04 10:32:17 -0500

    Add Updated Test
    Completed Assignments in FCC and updated test from Replit

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,8 +15,8 @@ suite('Functional Tests', function () {
         .request(server)
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -24,10 +24,10 @@ suite('Functional Tests', function () {
     test('Test GET /hello with your name', function (done) {
       chai
         .request(server)
-        .get('/hello?name=xy_z')
+        .get('/hello?name=Jerry')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Jerry');
           done();
         });
     });
@@ -36,28 +36,51 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-
+        .send({
+          "surname": "Colombo"
+        })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200, "response status should be 200");
+          assert.equal(res.type, 'application/json', 'response should be in json');
+          assert.equal(res.body.name, 
+                       'Cristoforo', 'resonse res.body.name should be Cristoforo');
+          assert.equal(res.body.surname, 'Colombo', 
+                       'response res.body.surname should be colombo');
           done();
         });
     });
-    // #4
+    
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai
+      .request(server)
+      .put('/travellers')
+      .send({
+        "surname": "da Verrazzano"
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'res.status should have the value 200');
+        assert.equal(res.type, "application/json", 
+                     "response.type should have value of json")
+        assert.equal(res.body.name, "Giovanni", 
+                    'res.body.name should have the value "Giovannie"')
+        assert.equal(res.body.surname, "da Verrazzano",
+                     'res.body.surname should have the value "da Verrazzano"')
+        done();
+      });
     });
   });
 });
 
 const Browser = require('zombie');
+Browser.site = 'https://boilerplate-mochachai.jerryh-jr.repl.co';
 
 suite('Functional Tests with Zombie.js', function () {
-  this.timeout(5000);
-
-
+  this.timeout(10000);
+  const browser = new Browser();
+  
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
@@ -66,17 +89,27 @@ suite('Functional Tests with Zombie.js', function () {
   });
 
   suite('"Famous Italian Explorers" form', function () {
-    // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill("surname", "Colombo" ).then(() => { browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+           done();
+        });
+      });
     });
-    // #6
-    test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
 
-      done();
+    test('Submit the surname "Vespucci" in the HTML form', function (done) {
+      broswer.fill('surname', 'Vespucci').then(() => { broweser.pressButton('submit', () => {
+        broswer.assert.success();
+        browser.assert.text('apan#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.elements('span#dates', 1);
+         done();
+        });                                           
+      });
     });
   });
 });
+
